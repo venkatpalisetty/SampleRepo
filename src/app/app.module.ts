25494cceb09a1db11ab005a6bc4353d5a403230c
srinu1134 <srinud030@gmail.com>
2024-01-24 10:48:01 +0530

    added app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JavaComponent } from './java/java.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JavaComponent,
   ],
   imports: [
     BrowserModule,
