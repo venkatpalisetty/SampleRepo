e2e475859a92d8606796a53dfb8c221dd65d837e
Bahatamkotamraju <bahatamkotamraju679@gmail.com>
2024-01-24 10:58:34 +0530

    add the component name done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitComponent } from './git/git.component';
+import { DoneComponent } from './done/done.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitComponent,
+    DoneComponent
   ],
   imports: [
     BrowserModule,
